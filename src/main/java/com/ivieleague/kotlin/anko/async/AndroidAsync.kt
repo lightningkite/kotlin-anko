819c7e82d79b2d1b6839b0f6d42dbd7a24f83b49
jivie <joseph@lightningkite.com>
2016-06-01 14:04:29 -0600

    Continuing the separation process

--- src/main/java/com/ivieleague/kotlin/anko/async/AndroidAsync.kt
+++ src/main/java/com/ivieleague/kotlin/anko/async/AndroidAsync.kt
@@ -3,41 +3,21 @@ package com.ivieleague.kotlin.anko.async
 import android.os.Handler
 import android.os.Looper
 import com.ivieleague.kotlin.async.Async
+import com.ivieleague.kotlin.async.AsyncInterface
 
 /**
  * Various functions to do things asynchronously.
  * Created by jivie on 9/2/15.
  */
 
-object AndroidAsync {
+object AndroidAsync : AsyncInterface {
     val uiHandler: Handler = Handler(Looper.getMainLooper())
-}
 
+    override fun sendToThread(action: () -> Unit) {
+        uiHandler.post(action)
+    }
 
-/**
- * Runs [action] asynchronously with its result being dealt with on the UI thread in [uiThread].
- * @param action The lambda to run asynchronously.
- * @param uiThread The lambda to run with the result of [action] on the UI thread.
- */
-fun <T> doAsync(action: () -> T, uiThread: (T) -> Unit) {
-    Async.threadPool.execute({
-        try {
-            val result = action()
-            AndroidAsync.uiHandler.post {
-                uiThread(result)
-            }
-        } catch(e: Exception) {
-            AndroidAsync.uiHandler.post {
-                throw e
-            }
-        }
-    })
-}
-
-/**
- * Posts [action] to the main thread.
- * @param action The lambda to run asynchronously.
- */
-fun doUiThread(delay: Long = 0, action: () -> Unit) {
-    AndroidAsync.uiHandler.postDelayed(action, delay)
+    fun init() {
+        Async.uiThreadInterface = this
+    }
 }
