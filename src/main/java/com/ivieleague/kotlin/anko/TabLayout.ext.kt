f16692a772748c1f4c994d98e08bc78f00a13eb7
jivie <joseph@lightningkite.com>
2016-06-10 10:14:41 -0600

    Many bug fixes and style updates

--- src/main/java/com/ivieleague/kotlin/anko/TabLayout.ext.kt
+++ src/main/java/com/ivieleague/kotlin/anko/TabLayout.ext.kt
@@ -4,12 +4,19 @@ import android.support.design.widget.TabLayout
 import org.jetbrains.anko.sp
 
 /**
+ * Various functions that are missing from TabLayout to use it programmatically.
+ *
  * Created by jivie on 4/28/16.
  */
+
 inline fun TabLayout.setTabTextSize(sp: Float) {
-    //Give the middle finger to the designer of TabLayout for me.
-    //We're going to reflect our way into this mess.
     val field = TabLayout::class.java.getDeclaredField("mTabTextSize")
     field.isAccessible = true
     field.set(this, sp(sp))
+}
+
+inline fun TabLayout.setTabBackgroundResource(res: Int) {
+    val field = TabLayout::class.java.getDeclaredField("mTabBackgroundResId")
+    field.isAccessible = true
+    field.set(this, res)
 }
