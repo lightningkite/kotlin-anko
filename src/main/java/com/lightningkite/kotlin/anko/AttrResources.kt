6cb1f44f7c407647534beab383938a34c939df92
jivie <joseph@lightningkite.com>
2016-07-11 16:21:46 -0600

    UI Updates

--- src/main/java/com/lightningkite/kotlin/anko/AttrResources.kt
+++ src/main/java/com/lightningkite/kotlin/anko/AttrResources.kt
@@ -12,8 +12,8 @@ val View.selectableItemBackgroundResource: Int
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             // If we're running on Honeycomb or newer, then we can use the Theme's
             // selectableItemBackground to ensure that the View has a pressed state
-            val outValue = TypedValue();
-            context.theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true);
+            val outValue = TypedValue()
+            context.theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
             return outValue.resourceId
         }
         return 0
@@ -24,8 +24,8 @@ val View.selectableItemBackgroundBorderlessResource: Int
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // If we're running on Honeycomb or newer, then we can use the Theme's
             // selectableItemBackground to ensure that the View has a pressed state
-            val outValue = TypedValue();
-            context.theme.resolveAttribute(android.R.attr.selectableItemBackgroundBorderless, outValue, true);
+            val outValue = TypedValue()
+            context.theme.resolveAttribute(android.R.attr.selectableItemBackgroundBorderless, outValue, true)
             return outValue.resourceId
         }
         return 0
