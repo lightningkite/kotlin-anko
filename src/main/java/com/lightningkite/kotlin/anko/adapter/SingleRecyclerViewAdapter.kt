bd6695515debddc1057c87a5aeb09ae2735be9e5
Joseph Ivie <josephivie@gmail.com>
2016-09-20 12:54:56 -0600

    New recyclerview stuff, updated design

---
+++ src/main/java/com/lightningkite/kotlin/anko/adapter/SingleRecyclerViewAdapter.kt
@@ -0,0 +1,38 @@
+package com.lightningkite.kotlin.anko.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import org.jetbrains.anko.AnkoContextImpl
+
+/**
+ * Created by joseph on 9/20/16.
+ */
+class SingleRecyclerViewAdapter(
+        val makeView: SRVAContext.() -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun getItemCount(): Int = 1
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return object : RecyclerView.ViewHolder(SRVAContext(this, parent.context).apply { makeView() }.view) {}
+    }
+
+    class SRVAContext(adapter: SingleRecyclerViewAdapter, context: Context) : AnkoContextImpl<SingleRecyclerViewAdapter>(context, adapter, false) {
+        fun <V : View> V.lparams(
+                width: Int = ViewGroup.LayoutParams.WRAP_CONTENT,
+                height: Int = ViewGroup.LayoutParams.WRAP_CONTENT,
+                init: RecyclerView.LayoutParams.() -> Unit = {}
+        ): V {
+            val layoutParams = RecyclerView.LayoutParams(width, height)
+            layoutParams.init()
+            this@lparams.layoutParams = layoutParams
+
+            return this
+        }
+    }
+}
+
+fun RecyclerView.singleAdapter(makeView: SingleRecyclerViewAdapter.SRVAContext.() -> Unit) = SingleRecyclerViewAdapter(makeView)
