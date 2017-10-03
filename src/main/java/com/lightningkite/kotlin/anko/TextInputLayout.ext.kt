9c19e9f477a39520199065ecbbf5ad75f715dcc3
Joseph Ivie <josephivie@gmail.com>
2017-10-03 11:56:13 -0600

    Restyling

--- src/main/java/com/lightningkite/kotlin/anko/TextInputLayout.ext.kt
+++ src/main/java/com/lightningkite/kotlin/anko/TextInputLayout.ext.kt
@@ -1,5 +1,6 @@
 package com.lightningkite.kotlin.anko
 
+import android.support.annotation.StyleRes
 import android.support.design.widget.TextInputEditText
 import android.support.design.widget.TextInputLayout
 import org.jetbrains.anko.custom.ankoView
@@ -21,3 +22,7 @@ var TextInputLayout.errorResource: Int
 inline fun TextInputLayout.textInputEditText(init: TextInputEditText.() -> Unit) = ankoView({
     TextInputEditText(it)
 }, 0, init)
+
+inline fun TextInputLayout.themedTextInputEditText(@StyleRes theme: Int, init: TextInputEditText.() -> Unit) = ankoView({
+    TextInputEditText(it)
+}, theme, init)
