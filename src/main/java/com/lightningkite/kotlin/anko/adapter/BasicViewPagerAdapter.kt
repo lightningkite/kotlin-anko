f9af2c62399e4ba60fb4d7e2627b6f9362391b6e
Joseph Ivie <josephivie@gmail.com>
2017-11-02 16:35:18 -0600

    Version update

--- src/main/java/com/lightningkite/kotlin/anko/adapter/BasicViewPagerAdapter.kt
+++ src/main/java/com/lightningkite/kotlin/anko/adapter/BasicViewPagerAdapter.kt
@@ -16,13 +16,13 @@ open class BasicViewPagerAdapter(val makeView: ViewGroup.(Int) -> View) : PagerA
         return makeView(container, position)
     }
 
-    override fun destroyItem(container: ViewGroup, position: Int, item: Any?) {
+    override fun destroyItem(container: ViewGroup, position: Int, item: Any) {
         container.removeView(item as View)
     }
 
     override fun getCount(): Int = itemCount
 
-    override fun isViewFromObject(view: View?, item: Any?): Boolean {
+    override fun isViewFromObject(view: View, item: Any): Boolean {
         return view == item
     }
 }
