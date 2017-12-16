66e0c1acda5bca3d7b1fa88b55b60ac8f2954ba4
iverc <ivan@pasturemap.com>
2017-12-17 00:05:53 +0200

    Update support libraries to 27.0.2

--- src/main/java/com/lightningkite/kotlin/anko/adapter/BasicViewPagerAdapter.kt
+++ src/main/java/com/lightningkite/kotlin/anko/adapter/BasicViewPagerAdapter.kt
@@ -9,20 +9,15 @@ import android.view.ViewGroup
  * Created by joseph on 11/16/16.
  */
 open class BasicViewPagerAdapter(val makeView: ViewGroup.(Int) -> View) : PagerAdapter() {
-
     var itemCount: Int = 0
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
         return makeView(container, position)
     }
 
-    override fun destroyItem(container: ViewGroup, position: Int, item: Any?) {
-        container.removeView(item as View)
-    }
-
     override fun getCount(): Int = itemCount
 
-    override fun isViewFromObject(view: View?, item: Any?): Boolean {
+    override fun isViewFromObject(view: View, item: Any): Boolean {
         return view == item
     }
 }
