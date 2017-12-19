62da2aed79beca7d259e7e3c40c14f7f27b6e55d
Joseph Ivie <josephivie@gmail.com>
2017-12-19 13:59:30 -0700

    Initial commit

--- src/main/java/com/lightningkite/kotlin/anko/FullInputType.kt
+++ src/main/java/com/lightningkite/kotlin/anko/FullInputType.kt
@@ -19,4 +19,5 @@ object FullInputType {
     const val SINGLE_LINE_SENTENCE: Int = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_SENTENCES
     const val ADDRESS: Int = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_POSTAL_ADDRESS or InputType.TYPE_TEXT_FLAG_CAP_WORDS
     const val URL: Int = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_URI
+    const val CODE: Int = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
 }
